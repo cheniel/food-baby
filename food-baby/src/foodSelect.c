687cccd062a4f3f8b50d75bab57111b31f4f98d5
cheniel <danielchen627@gmail.com>
2014-08-27 01:51:03 -0400

    started data and serving stuff

--- food-baby/src/foodSelect.c
+++ food-baby/src/foodSelect.c
@@ -15,20 +15,37 @@
 #include "common.h"
 
 // ---------------- Constant definitions
-// static const int = 5
+char* servingSizeGrains[] = {
+  "1 slice of bread",
+};
+
+char* servingSizeVegetables[] = {
+  "1 cup of broccoli",
+};
+
+char* servingSizeFruits[] = {
+  "1/2 a large apple",
+  "a banana",
+};
+
+char* servingSizeDairy[] = {
+  "a cup of milk",
+};
+
+char* servingSizeMeat[] = {
+  "3 oz of meat",
+};
 
 // ---------------- Macro definitions
-#define NUM_MENU_SECTIONS 2
-#define NUM_FIRST_MENU_ITEMS 6
-#define NUM_SECOND_MENU_ITEMS 1
+#define NUM_MENU_SECTIONS 1
+#define NUM_MENU_ITEMS 5
 
 // ---------------- Structures/Types
 
 // ---------------- Private variables
 static SimpleMenuLayer *simple_menu_layer;
 static SimpleMenuSection menu_sections[NUM_MENU_SECTIONS];
-static SimpleMenuItem first_menu_items[NUM_FIRST_MENU_ITEMS];
-static SimpleMenuItem second_menu_items[NUM_SECOND_MENU_ITEMS];
+static SimpleMenuItem menu_items[NUM_MENU_ITEMS];
 
 // ---------------- Private prototypes
 static void load(Window *window);
@@ -49,76 +66,51 @@ Window *foodInit() {
 // You can capture when the user selects a menu icon with a menu item select callback
 static void menu_select_callback(int index, void *ctx) {
   // Here we just change the subtitle to a literal string
-  first_menu_items[index].subtitle = "You've hit select here!";
+  menu_items[index].subtitle = "You've hit select here!";
   // Mark the layer to be updated
   layer_mark_dirty(simple_menu_layer_get_layer(simple_menu_layer));
 }
 
 static void load(Window *window) {
-
-  // Although we already defined NUM_FIRST_MENU_ITEMS, you can define
-  // an int as such to easily change the order of menu items later
   int num_a_items = 0;
 
-  // This is an example of how you'd set a simple menu item
-  first_menu_items[num_a_items++] = (SimpleMenuItem){
-    // You should give each menu item a title and callback
+  menu_items[num_a_items++] = (SimpleMenuItem){
     .title = "Grains",
-    .subtitle = "another",
+    .subtitle = "e.g. 1 slice of bread",
     .callback = menu_select_callback,
   };
 
-  // The menu items appear in the order saved in the menu items array
-  first_menu_items[num_a_items++] = (SimpleMenuItem){
+  menu_items[num_a_items++] = (SimpleMenuItem){
     .title = "Vegetable",
-    // You can also give menu items a subtitle
-    .subtitle = "Here's a subtitle",
+    .subtitle = "e.g. 1 cup of broccoli",
     .callback = menu_select_callback,
   };
 
-  first_menu_items[num_a_items++] = (SimpleMenuItem){
+  menu_items[num_a_items++] = (SimpleMenuItem){
     .title = "Fruit",
-    .subtitle = "This has an icon",
+    .subtitle = "e.g. 1/2 a large apple",
     .callback = menu_select_callback,
     // This is how you would give a menu item an icon
     // .icon = menu_icon_image,
   };
 
-  first_menu_items[num_a_items++] = (SimpleMenuItem){
+  menu_items[num_a_items++] = (SimpleMenuItem){
     .title = "Dairy",
-    .subtitle = "This has an icon",
+    .subtitle = "e.g. a cup of milk",
     .callback = menu_select_callback,
-    // This is how you would give a menu item an icon
-    // .icon = menu_icon_image,
   };
 
-  first_menu_items[num_a_items++] = (SimpleMenuItem){
-    .title = "Meat",
-    .subtitle = "This has an icon",
+  menu_items[num_a_items++] = (SimpleMenuItem){
+    .title = "Protein",
+    .subtitle = "e.g. 3 oz of meat",
     .callback = menu_select_callback,
-    // This is how you would give a menu item an icon
-    // .icon = menu_icon_image,
-  };
-
-  // This initializes the second section
-  second_menu_items[0] = (SimpleMenuItem){
-    .title = "Special Item",
-    // You can use different callbacks for your menu items
-    .callback = menu_select_callback
   };
 
   // Bind the menu items to the corresponding menu sections
   menu_sections[0] = (SimpleMenuSection){
     .title = "Log your servings!",
-    .num_items = NUM_FIRST_MENU_ITEMS,
-    .items = first_menu_items,
-  };
-
-  menu_sections[1] = (SimpleMenuSection){
-    // Menu sections can also have titles as well
-    .title = "Yet Another Section",
-    .num_items = NUM_SECOND_MENU_ITEMS,
-    .items = second_menu_items,
+    .num_items = NUM_MENU_ITEMS,
+    .items = menu_items,
   };
 
   // Now we prepare to initialize the simple menu layer
