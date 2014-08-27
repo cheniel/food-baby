e1950522ccde561327c8659e4de5d04fcb067271
cheniel <danielchen627@gmail.com>
2014-08-27 01:07:34 -0400

    started foodSelect menu

--- food-baby/src/foodSelect.c
+++ food-baby/src/foodSelect.c
@@ -18,18 +18,19 @@
 // static const int = 5
 
 // ---------------- Macro definitions
-// #define CONSTANT_VAR 42
+#define NUM_MENU_SECTIONS 2
+#define NUM_FIRST_MENU_ITEMS 6
+#define NUM_SECOND_MENU_ITEMS 1
 
 // ---------------- Structures/Types
 
 // ---------------- Private variables
-static TextLayer *text_layer;
+static SimpleMenuLayer *simple_menu_layer;
+static SimpleMenuSection menu_sections[NUM_MENU_SECTIONS];
+static SimpleMenuItem first_menu_items[NUM_FIRST_MENU_ITEMS];
+static SimpleMenuItem second_menu_items[NUM_SECOND_MENU_ITEMS];
 
 // ---------------- Private prototypes
-static void select_click_handler(ClickRecognizerRef recognizer, void *context);
-static void up_click_handler(ClickRecognizerRef recognizer, void *context);
-static void down_click_handler(ClickRecognizerRef recognizer, void *context);
-static void click_config_provider(void *context);
 static void load(Window *window);
 static void unload(Window *window);
 
@@ -37,7 +38,6 @@ static void unload(Window *window);
 
 Window *foodInit() {
 	Window *window = window_create();
-	window_set_click_config_provider(window, click_config_provider);
 	window_set_window_handlers(window, (WindowHandlers) {
 	  .load = load,
 	  .unload = unload,
@@ -46,42 +46,96 @@ Window *foodInit() {
 	return window;
 }
 
-static void select_click_handler(ClickRecognizerRef recognizer, void *context) {
-  text_layer_set_text(text_layer, "Select");
-
-  // increment water count
-
-}
-
-static void up_click_handler(ClickRecognizerRef recognizer, void *context) {
-  text_layer_set_text(text_layer, "Up");
-
-  // load food select window
-}
-
-static void down_click_handler(ClickRecognizerRef recognizer, void *context) {
-  text_layer_set_text(text_layer, "Down");
-
-  // load log window
-}
-
-static void click_config_provider(void *context) {
-  window_single_click_subscribe(BUTTON_ID_SELECT, select_click_handler);
-  window_single_click_subscribe(BUTTON_ID_UP, up_click_handler);
-  window_single_click_subscribe(BUTTON_ID_DOWN, down_click_handler);
+// You can capture when the user selects a menu icon with a menu item select callback
+static void menu_select_callback(int index, void *ctx) {
+  // Here we just change the subtitle to a literal string
+  first_menu_items[index].subtitle = "You've hit select here!";
+  // Mark the layer to be updated
+  layer_mark_dirty(simple_menu_layer_get_layer(simple_menu_layer));
 }
 
 static void load(Window *window) {
-  Layer *window_layer = window_get_root_layer(window);
-  GRect bounds = layer_get_bounds(window_layer);
 
-  text_layer = text_layer_create((GRect) { .origin = { 0, 72 }, .size = { bounds.size.w, 20 } });
-  text_layer_set_text(text_layer, "food select");
-  text_layer_set_text_alignment(text_layer, GTextAlignmentCenter);
-  layer_add_child(window_layer, text_layer_get_layer(text_layer));
+  // Although we already defined NUM_FIRST_MENU_ITEMS, you can define
+  // an int as such to easily change the order of menu items later
+  int num_a_items = 0;
+
+  // This is an example of how you'd set a simple menu item
+  first_menu_items[num_a_items++] = (SimpleMenuItem){
+    // You should give each menu item a title and callback
+    .title = "Grains",
+    .subtitle = "another",
+    .callback = menu_select_callback,
+  };
+
+  // The menu items appear in the order saved in the menu items array
+  first_menu_items[num_a_items++] = (SimpleMenuItem){
+    .title = "Vegetable",
+    // You can also give menu items a subtitle
+    .subtitle = "Here's a subtitle",
+    .callback = menu_select_callback,
+  };
+
+  first_menu_items[num_a_items++] = (SimpleMenuItem){
+    .title = "Fruit",
+    .subtitle = "This has an icon",
+    .callback = menu_select_callback,
+    // This is how you would give a menu item an icon
+    // .icon = menu_icon_image,
+  };
+
+  first_menu_items[num_a_items++] = (SimpleMenuItem){
+    .title = "Dairy",
+    .subtitle = "This has an icon",
+    .callback = menu_select_callback,
+    // This is how you would give a menu item an icon
+    // .icon = menu_icon_image,
+  };
+
+  first_menu_items[num_a_items++] = (SimpleMenuItem){
+    .title = "Meat",
+    .subtitle = "This has an icon",
+    .callback = menu_select_callback,
+    // This is how you would give a menu item an icon
+    // .icon = menu_icon_image,
+  };
+
+  // This initializes the second section
+  second_menu_items[0] = (SimpleMenuItem){
+    .title = "Special Item",
+    // You can use different callbacks for your menu items
+    .callback = menu_select_callback
+  };
+
+  // Bind the menu items to the corresponding menu sections
+  menu_sections[0] = (SimpleMenuSection){
+    .title = "Log your servings!",
+    .num_items = NUM_FIRST_MENU_ITEMS,
+    .items = first_menu_items,
+  };
+
+  menu_sections[1] = (SimpleMenuSection){
+    // Menu sections can also have titles as well
+    .title = "Yet Another Section",
+    .num_items = NUM_SECOND_MENU_ITEMS,
+    .items = second_menu_items,
+  };
+
+  // Now we prepare to initialize the simple menu layer
+  // We need the bounds to specify the simple menu layer's viewport size
+  // In this case, it'll be the same as the window's
+  Layer *window_layer = window_get_root_layer(window);
+  GRect bounds = layer_get_frame(window_layer);
+
+  // Initialize the simple menu layer
+  simple_menu_layer = simple_menu_layer_create(bounds, window, menu_sections, NUM_MENU_SECTIONS, NULL);
+
+  // Add it to the window for display
+  layer_add_child(window_layer, simple_menu_layer_get_layer(simple_menu_layer));
+
 }
 
 static void unload(Window *window) {
-  text_layer_destroy(text_layer);
+  simple_menu_layer_destroy(simple_menu_layer);
 }
 
