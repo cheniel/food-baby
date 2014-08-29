a680585447ebc6b73965f635245c88351c68fb82
cheniel <danielchen627@gmail.com>
2014-08-28 20:27:36 -0400

    added reset data options

--- food-baby/src/foodSelect.c
+++ food-baby/src/foodSelect.c
@@ -72,18 +72,20 @@ char* servingSizeProteins[] = {
 };
 
 // ---------------- Macro definitions
-#define NUM_MENU_SECTIONS 1
+#define NUM_MENU_SECTIONS 2
 #define NUM_FOOD_GROUPS 5
 #define NUM_SERVING_EXAMPLES 8
+#define NUM_SETTINGS 2
 #define MAX_COMPARE_SIZE 10
 #define ANIMATED_SETTING true
 
 // ---------------- Structures/Types
 
 // ---------------- Private variables
-static SimpleMenuLayer *simple_menu_layer;
-static SimpleMenuSection menu_sections[NUM_MENU_SECTIONS];
-static SimpleMenuItem FOOD_GROUPS[NUM_FOOD_GROUPS];
+static SimpleMenuLayer *menuLayer;
+static SimpleMenuSection menuSections[NUM_MENU_SECTIONS];
+static SimpleMenuItem foodGroups[NUM_FOOD_GROUPS];
+static SimpleMenuItem settings[NUM_SETTINGS];
 
 extern ServingCount userServings;
 
@@ -139,55 +141,74 @@ static void load(Window *window) {
   Layer *window_layer = window_get_root_layer(window);
   GRect bounds = layer_get_frame(window_layer);
 
-  int num_a_items = 0;
+  int menuItem = 0;
 
-  FOOD_GROUPS[num_a_items++] = (SimpleMenuItem){
+  foodGroups[menuItem++] = (SimpleMenuItem) {
     .title = "grains",
     .subtitle = getServingExample("grains"),
     .callback = grainSelectCallback,
   };
 
-  FOOD_GROUPS[num_a_items++] = (SimpleMenuItem){
+  foodGroups[menuItem++] = (SimpleMenuItem) {
     .title = "vegetables",
     .subtitle = getServingExample("vegetables"),
     .callback = vegetableSelectCallback,
   };
 
-  FOOD_GROUPS[num_a_items++] = (SimpleMenuItem){
+  foodGroups[menuItem++] = (SimpleMenuItem) {
     .title = "fruits",
     .subtitle = getServingExample("fruits"),
     .callback = fruitSelectCallback,
   };
 
-  FOOD_GROUPS[num_a_items++] = (SimpleMenuItem){
+  foodGroups[menuItem++] = (SimpleMenuItem) {
     .title = "dairy",
     .subtitle = getServingExample("dairy"),
     .callback = dairySelectCallback,
   };
 
-  FOOD_GROUPS[num_a_items++] = (SimpleMenuItem){
+  foodGroups[menuItem++] = (SimpleMenuItem) {
     .title = "proteins",
     .subtitle = getServingExample("proteins"),
     .callback = proteinSelectCallback,
   };
 
+  menuItem = 0;
+  settings[menuItem++] = (SimpleMenuItem) {
+    .title = "reset daily data",
+    .subtitle = "eating and activity data",
+    .callback = resetDailyData,
+  };
+
+  settings[menuItem++] = (SimpleMenuItem) {
+    .title = "reset record",
+    .subtitle = "activity record",
+    .callback = resetRecord,
+  };
+
   // Bind the menu items to the corresponding menu sections
-  menu_sections[0] = (SimpleMenuSection){
+  menuSections[0] = (SimpleMenuSection){
     .title = "log your servings!",
     .num_items = NUM_FOOD_GROUPS,
-    .items = FOOD_GROUPS,
+    .items = foodGroups,
+  };
+
+  menuSections[1] = (SimpleMenuSection) {
+    .title = "settings",
+    .num_items = NUM_SETTINGS,
+    .items = settings,
   };
 
   // Initialize the simple menu layer
-  simple_menu_layer = simple_menu_layer_create(bounds, window, menu_sections, NUM_MENU_SECTIONS, NULL);
+  menuLayer = simple_menu_layer_create(bounds, window, menuSections, NUM_MENU_SECTIONS, NULL);
 
   // Add it to the window for display
-  layer_add_child(window_layer, simple_menu_layer_get_layer(simple_menu_layer));
+  layer_add_child(window_layer, simple_menu_layer_get_layer(menuLayer));
 
 }
 
 static void unload(Window *window) {
-  simple_menu_layer_destroy(simple_menu_layer);
+  simple_menu_layer_destroy(menuLayer);
 }
 
 
