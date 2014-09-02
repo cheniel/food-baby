5fc0b7ea8b4315db80a769569b37f41fd916d792
cheniel <danielchen627@gmail.com>
2014-09-02 01:51:58 -0400

    added basic sleep animation

--- food-baby/src/sprite.c
+++ food-baby/src/sprite.c
@@ -31,6 +31,10 @@
 #define SPRITE_XMIN 0
 #define SPRITE_XMAX PEBBLE_WIDTH - SPRITE_WIDTH
 
+#define SLEEP_COUNT 3
+
+#define ANIMATION_DURATION_MS 7000
+
 // ---------------- Structures/Types
 typedef struct SpriteInfo {
     int x;
@@ -43,10 +47,25 @@ static BitmapLayer *spriteLayer;
 static GBitmap *spriteImg;
 static Layer* window;
 static SpriteInfo baby;
+static Animation *sleepAnim;
+
+static int sleepCounter;
+static TextLayer *sleepZZZs[SLEEP_COUNT];
 
 // ---------------- Private prototypes
 static void createSprite();
 static void startAnimation();
+static void sleepAnimSetup(struct Animation *animation);
+static void sleepAnimUpdate(struct Animation *animation, 
+    const uint32_t time_normalized);
+static void sleepAnimTeardown(struct Animation *animation);
+
+// ----------------- Animation Structures
+static const AnimationImplementation sleepAnimImpl = {
+    .setup = sleepAnimSetup,
+    .update = sleepAnimUpdate,
+    .teardown = sleepAnimTeardown,
+};
 
 /* ========================================================================== */
 
@@ -74,21 +93,98 @@ static void createSprite() {
 static void startAnimation() {
     baby.state = getSpriteState();
 
+    APP_LOG(APP_LOG_LEVEL_DEBUG, "determining baby state: ");
+
+    sleepAnim = animation_create();
+
     switch (baby.state) {
         case spriteAsleep:
+            APP_LOG(APP_LOG_LEVEL_DEBUG, "\tspriteAsleep");
+            animation_set_duration(sleepAnim, ANIMATION_DURATION_MS);
+            animation_set_implementation(sleepAnim, &sleepAnimImpl);
             break;
         case spriteSad:
+            APP_LOG(APP_LOG_LEVEL_DEBUG, "\tspriteSad");
+            animation_set_duration(sleepAnim, ANIMATION_DURATION_MS);
+            animation_set_implementation(sleepAnim, &sleepAnimImpl);
             break;
         case spriteContent:
+            APP_LOG(APP_LOG_LEVEL_DEBUG, "\tspriteContent");
+            animation_set_duration(sleepAnim, ANIMATION_DURATION_MS);
+            animation_set_implementation(sleepAnim, &sleepAnimImpl);
             break;
         case spriteHappy:
+            APP_LOG(APP_LOG_LEVEL_DEBUG, "\tspriteHappy");
+            animation_set_duration(sleepAnim, ANIMATION_DURATION_MS);
+            animation_set_implementation(sleepAnim, &sleepAnimImpl);
             break;
         default:
             APP_LOG(APP_LOG_LEVEL_ERROR, "startAnimation: INVALID STATE");
             break;
     }
 
+    animation_schedule(sleepAnim);
+}
+
+static void sleepAnimSetup(struct Animation *animation) {
+
+    // set up ZZZs
+    sleepZZZs[0] = text_layer_create((GRect) {
+        .origin = { 90, 80 },
+        .size = { 15, 15 }
+    });
+
+    sleepZZZs[1] = text_layer_create((GRect) {
+        .origin = { 100, 70 },
+        .size = { 15, 15 }
+    });
+
+    sleepZZZs[2] = text_layer_create((GRect) {
+        .origin = { 110, 60 },
+        .size = { 15, 15 }
+    });
+
+    for (int z = 0; z < SLEEP_COUNT; z++) {
+        text_layer_set_text(sleepZZZs[z], "z");
+        setTextLayerDefaults(sleepZZZs[z]);
+
+    }
+
+    // move baby to center
+    baby.x = SPRITE_STARTX;
+    baby.y = SPRITE_STARTY;
+
+    // change icon to sleeping
+
+    sleepCounter = 0;
+}
+
+static void sleepAnimUpdate(struct Animation *animation, 
+    const uint32_t time_normalized) {
     
+    if (sleepCounter >= SLEEP_COUNT) {
+        sleepCounter = 0;
+
+        // remove all displayed Zs
+        for (int z = 0; z < SLEEP_COUNT; z++) {
+            layer_remove_from_parent(text_layer_get_layer(sleepZZZs[z]));
+        }   
+
+    } else {
+
+        // make Z at sleepCounter visible 
+        layer_add_child(window, text_layer_get_layer(sleepZZZs[sleepCounter]));
+
+        sleepCounter++;
+    }
+
+    psleep(1000);
+}
+
+static void sleepAnimTeardown(struct Animation *animation) {
+    for (int z = 0; z < SLEEP_COUNT; z++) { text_layer_destroy(sleepZZZs[z]); }
+
+    startAnimation();
 }
 
 void deinitSprite() {
